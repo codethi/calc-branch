fe8545eccfb51f6327f97e59787034d71ca8c20c
Thiago Lima <50744116+codethi@users.noreply.github.com>
2023-01-20 21:24:25 +0000

    feat: create multiply function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
 
 console.log(subtract(2, 2));
 console.log(sum(2, 2));
+console.log(multiply(2, 2));
